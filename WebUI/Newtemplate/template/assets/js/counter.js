95eea3ee9b2fe34226aa58f9236d9a6bde67f509
Nahla <prog05@bse.com.eg>
2022-06-26 14:27:33 +0200

    Nahla

--- WebUI/Newtemplate/template/assets/js/counter.js
+++ WebUI/Newtemplate/template/assets/js/counter.js
@@ -1,17 +1,26 @@
-﻿var decreaseButton = document.getElementById("decrease");
-var increaseButton = document.getElementById("increase");
+﻿//var decreaseButton = document.getElementById("decrease");
+//var increaseButton = document.getElementById("increase");
+var decreaseButton = document.querySelector('[data-id="decrease"]');
+var increaseButton = document.querySelector('[data-id="increase"]');
+if (decreaseButton != null) {
+    decreaseButton.addEventListener("click", () => {
+        var value = parseInt(document.querySelector('[data-id="number"]').value, 10);
+        value = isNaN(value) ? 0 : value;
+        value < 1 ? value = 1 : '';
+        value--;
+        document.querySelector('[data-id="number"]').value = value;
+    });
+}
 
-decreaseButton.addEventListener("click", () => {
-    var value = parseInt(document.querySelector('[data-id="number"]').value, 10);
-    value = isNaN(value) ? 0 : value;
-    value < 1 ? value = 1 : '';
-    value--;
-    document.querySelector('[data-id="number"]').value = value;
-});
-
-increaseButton.addEventListener("click", () => {
-    var value = parseInt(document.querySelector('[data-id="number"]').value, 10);
-    value = isNaN(value) ? 0 : value;
-    value++;
-    document.querySelector('[data-id="number"]').value = value;
-});
+if (increaseButton != null) {
+    increaseButton.addEventListener("click", () => {
+        var value = parseInt(document.querySelector('[data-id="number"]').value, 10);
+        value = isNaN(value) ? 0 : value;
+        value++;
+        document.querySelector('[data-id="number"]').value = value;
+    });
+}
+/*Alert Message*/
+var myAlert = document.getElementById('alert-message');
+var bsAlert = new bootstrap.Toast(myAlert);
+bsAlert.show();
